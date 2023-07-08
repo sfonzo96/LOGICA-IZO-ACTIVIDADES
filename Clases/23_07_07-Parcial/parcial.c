cbc5d2ba37bbe499057a3392bf262d24646a40f7
santif <santiagofonzo@live.com>
2023-07-08 12:44:34 -0300

    [UPDATE] Mostrar historial
    - Se muestra el listado de cobros al finalizar la carga de datos

--- Clases/23_07_07-Parcial/parcial.c
+++ Clases/23_07_07-Parcial/parcial.c
@@ -20,27 +20,35 @@ void clearInputBuffer() {
 
 int main() {
 	float importeAuto, importeCamion, totalACobrar;
-	int tipoVehiculo, cantidadEjes, i = 0;
+	int tipoVehiculo, cantidadEjes, i = 0, opcionMenu;
 	bool cargaEstaTerminada = false;
 	
 	FILE* importeAutoTxt = fopen(".\\importe1.txt", "r");
 	FILE* importeCamionTxt = fopen(".\\importe2.txt", "r");
-	FILE* historialTxt = fopen(".\\historial.txt", "a");
+	FILE* historialTxtAppend = fopen(".\\historial.txt", "a");
+	FILE* historialTxtRead = fopen(".\\historial.txt", "r");
 	
 	setlocale(LC_CTYPE, "Spanish");
 
 	printf("Se inicia una nueva tanda de cobro de peajes.\n\n");
 	
-	if (importeAutoTxt == NULL || importeCamionTxt == NULL || historialTxt == NULL) {
+	// Verifica que se hayan encontrado los archivos
+	if (importeAutoTxt == NULL || importeCamionTxt == NULL || historialTxtAppend == NULL || historialTxtRead == NULL) {
 		printf("Uno de los archivos no se pudo abrir de manera exitosa. Se cerrará el programa.");
 		getch();
 		return 1;
 	}
 	
+	// Toma los valores de los archivos
 	fscanf(importeCamionTxt,"%f", &importeCamion);
 	fscanf(importeAutoTxt,"%f", &importeAuto);
 	
+	// Cierra la conexión a los archivos
+	fclose(importeAutoTxt);
+    fclose(importeCamionTxt);
+	
 	while (!cargaEstaTerminada) {
+		// Píde al usuario la categoría del vehículo
 		printf("Seleccione la categoría correspondiente al vehículo:\n1- Auto\n2- Camión\n");
 		while(scanf("%d", &tipoVehiculo) != 1 || tipoVehiculo < 0 || tipoVehiculo > 2) {
 			printf("El valor ingresado no es correcto, por favor ingresalo nuevamente...\n");
@@ -50,6 +58,7 @@ int main() {
 		
 		system("cls");
 		
+		// Pide al usuario la cantidad de ejes del vehículo
 		printf("Ingrese la cantidad de ejes del vehículo:");
 		while(scanf("%d", &cantidadEjes) != 1 || cantidadEjes < 0) {
 			printf("El valor ingresado no es correcto, por favor ingresalo nuevamente...\n");
@@ -60,10 +69,11 @@ int main() {
 		
 		system("cls");
 		
+		// Calcula el total a cobrar de acuerdo al tipo de vehículo
 		totalACobrar = (tipoVehiculo == 1? importeAuto : importeCamion) * cantidadEjes;
 		
-		fprintf(historialTxt, "%.2f", totalACobrar);
-		fprintf(historialTxt, "%c", '\n');
+		// Guarda el valor a cobrar en el historia y agrega un salto de línea
+		fprintf(historialTxtAppend, "%.2f\n", totalACobrar);
 		
 		printf("Se cobrarán $%.2f.\n\nPresione cualquier tecla para confirmar...\n", totalACobrar);
 		getch();
@@ -72,7 +82,7 @@ int main() {
 		
 		system("cls");
 		
-		int opcionMenu;
+		// Pide al usuario que elija como continuar
 		printf("Se registraron %i cobros. Seleccione como continuar:\n1- Seguir cobrando\n2- Finalizar\n", i);
 		while(scanf("%d", &opcionMenu) != 1 || opcionMenu < 0 || opcionMenu > 2) {
 			printf("El valor ingresado no es correcto, por favor ingresalo nuevamente...\n");
@@ -82,12 +92,22 @@ int main() {
 		
 		system("cls");
 		
+		// Si el usuario elige salir se cambia la condición que controla el bucle
 		if (opcionMenu == 2) {
 			cargaEstaTerminada = true;
 		}
 	}
 	
-	printf("Se ha finalizado la cobranza.");
+	printf("Se ha finalizado la cobranza.\n\n");
+	
+	printf("----COBROS REGISTRADOS AL CORRIENTE----\n");
+	float importe;
+    while(fscanf(historialTxtRead, "%f", &importe) == 1) {
+		printf("$\t%.2f\n", importe);
+	}
+	
+	fclose(historialTxtRead);
+	fclose(historialTxtAppend);
 	
 	return 0;
-}
+};
