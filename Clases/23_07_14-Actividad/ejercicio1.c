0e43c263904a2d0b4372b5c330cf18388c289a2b
santif <santiagofonzo@live.com>
2023-07-14 19:03:54 -0300

    [ADD] Archivos de la actividad del 14/07/2023
    - Agregado de actividad del 14/07/2023
    - Modificación detalles de resolución del parcial
    - Actualización de README.md

--- Clases/23_07_14-Actividad/ejercicio1.c
+++ Clases/23_07_14-Actividad/ejercicio1.c
@@ -1,5 +1,5 @@
 /*
-	Consigna parcial:
+	Consigna:
 	
 		Elaborar programa para cobro de peaje:
 			- Categoria auto = importe 1 (100)
@@ -7,33 +7,38 @@
 			- El importe se paga por eje de acuerdo a la categoria del vehiculo.
 			- Se debe mostrar el total a pagar y guardar en un historial.
 			- Se deben poder cargar tantos cobros como se quiera hasta que se decida finalizar.
+			- Se deben grabar también, la patente del vehículo, el tiempo y fecha del cobro y la categoría
 */
 
 #include <stdio.h>
 #include <locale.h>
 #include <conio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 void clearInputBuffer() {
 	while (getchar() != '\n');
 }
 
 int main() {
-	float importeAuto, importeCamion, totalACobrar, importeLista, totalPeriodo = 0;;
+	float importeAuto, importeCamion, totalACobrar, importeLista, totalPeriodo = 0;
 	int tipoVehiculo, cantidadEjes, i = 0, opcionMenu;
 	bool cargaEstaTerminada = false;
+	char patente[15];
+	time_t payTime = time(NULL);
 	
 	FILE* importeAutoTxt = fopen(".\\importe1.txt", "r");
 	FILE* importeCamionTxt = fopen(".\\importe2.txt", "r");
 	FILE* historialTxtAppend = fopen(".\\historial.txt", "a");
-	FILE* historialTxtRead = fopen(".\\historial.txt", "r");
 	
 	setlocale(LC_CTYPE, "Spanish");
 
 	printf("Se inicia una nueva tanda de cobro de peajes.\n\n");
 	
 	// Verifica que se hayan encontrado los archivos
-	if (importeAutoTxt == NULL || importeCamionTxt == NULL || historialTxtAppend == NULL || historialTxtRead == NULL) {
+	if (importeAutoTxt == NULL || importeCamionTxt == NULL || historialTxtAppend == NULL) {
 		printf("Uno de los archivos no se pudo abrir de manera exitosa. Se cerrará el programa.");
 		getch();
 		return 1;
@@ -48,9 +53,19 @@ int main() {
     fclose(importeCamionTxt);
 	
 	while (!cargaEstaTerminada) {
-		// Píde al usuario la categoría del vehículo
+		// Pide al usuario la categoría del vehículo
 		printf("Seleccione la categoría correspondiente al vehículo:\n1- Auto\n2- Camión\n");
 		while(scanf("%d", &tipoVehiculo) != 1 || tipoVehiculo < 0 || tipoVehiculo > 2) {
+			printf("El valor ingresado no es correcto, por favor ingresalo nuevamente...\n");
+			clearInputBuffer();
+		}
+		clearInputBuffer();
+		
+		system("cls");
+		
+		// Pide al usuario la patente del vehículo
+		printf("Ingrese la pantente del vehículo, sin espacios:\n");
+		while(scanf("%s", patente) != 1) {
 			printf("El valor ingresado no es correcto, por favor ingresalo nuevamente...\n");
 			clearInputBuffer();
 		}
@@ -65,21 +80,20 @@ int main() {
 			clearInputBuffer();
 		}
 		clearInputBuffer();
-		printf("%d", cantidadEjes);
-		
+ 		
 		system("cls");
 		
 		// Calcula el total a cobrar de acuerdo al tipo de vehículo
 		totalACobrar = (tipoVehiculo == 1? importeAuto : importeCamion) * cantidadEjes;
 		
 		// Guarda el valor a cobrar en el historia y agrega un salto de línea
-		fprintf(historialTxtAppend, "%.2f\n", totalACobrar);
+		fprintf(historialTxtAppend, "%.2f,%s,%s,%s", totalACobrar, tipoVehiculo == 1? "Auto" : "Camión", patente, asctime(localtime(&payTime)));
 		
 		printf("Se cobrarán $%.2f.\n\nPresione cualquier tecla para confirmar...\n", totalACobrar);
 		getch();
 		
 		i++;
-		
+			
 		system("cls");
 		
 		// Pide al usuario que elija como continuar
@@ -95,22 +109,37 @@ int main() {
 		// Si el usuario elige salir se cambia la condición que controla el bucle
 		if (opcionMenu == 2) {
 			cargaEstaTerminada = true;
-		}
-	}
+		};
+	};
 	
-	fclose(historialTxtAppend); // Cierra buffer de escritura para reflejar cambio en txt
 	printf("Se ha finalizado la cobranza.\n\n");
 	
-	// Muestra los registros de cobro almacenados
+	fclose(historialTxtAppend); // Cierra buffer de escritura para reflejar el cambio en el txt
+
+	// Abre un buffer de lectura de archivo
+	FILE* historialTxtRead = fopen(".\\historial.txt", "r");	
 	printf("----COBROS REGISTRADOS AL CORRIENTE----\n");
 	
-    while(fscanf(historialTxtRead, "%f", &importeLista) == 1) {
-		printf("$\t%.2f\n", importeLista);
-		totalPeriodo += importeLista;
-	}
+
+	char line[1024];
+	// Lee cada linea y la guarda en string line
+    while(fgets(line, sizeof(line), historialTxtRead) != NULL) {
+    	// Separa la linea en tokens usando un separador (","). Asigna el primer valor previo al separador a la variable token
+    	char *token = strtok(line, ",");
+		
+		// Verifica la existencia de valor en token
+		if (token != NULL) {
+		    importeLista = atof(token); // Convierte el token a float (string por defecto)
+		    printf("$\t%.2f\n", importeLista); // Imprime el importe
+		    totalPeriodo += importeLista; // Acumula el importe
+		}		
+		
+	};
 	printf("---------------------TOTAL:\n$\t%.2f", totalPeriodo);
 	
 	fclose(historialTxtRead);
+	fclose(historialTxtAppend);
 	
 	return 0;
 };
+
--- README.md
+++ README.md
@@ -239,6 +239,18 @@ La estructura de este repositorio se compone de dos directorios principales:
 
     Ver [archivo](https://github.com/sfonzo96/IZO-Logica-Actividades/blob/main/Clases/23_07_07-Parcial/parcial.c).
 
+### Clase 14-07-2023 (Uso de strings)
+
+-   Consigna:
+
+    Tomar ejercicio del parcial ([archivo parcial](https://github.com/sfonzo96/IZO-Logica-Actividades/blob/main/Clases/23_07_07-Parcial/parcial.c)) y agregar funciones:
+
+    -   Se debe pedir y almacenar la patente del vehículo
+    -   Se debe almacenar la fecha y hora del cobro
+    -   Se debe almacenar la categoría del vehículo
+
+    Ver [archivo](https://github.com/sfonzo96/IZO-Logica-Actividades/blob/main/Clases/23_07_14-Actividad/ejercicio1.c).
+
 ## Ejercicitación del libro
 
 ### Capitulo 1
