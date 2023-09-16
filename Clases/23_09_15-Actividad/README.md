b7263f79876b1cac127710084e377c9f20418131
santif <santiagofonzo@live.com>
2023-09-16 16:39:30 -0300

    [ADD] Ejercitación 15/09

---
+++ Clases/23_09_15-Actividad/README.md
@@ -0,0 +1,7 @@
+# Lógica y estructura de datos - IZO (2023)
+
+### Clase 15-09-2023
+
+-   Ejercicio 1
+
+    Se precisa un sistema básico de gestión de alumnos donde se puedan visualizar un listado de alumnos, además se deben poder agregar, editar y eliminar (CRUD). Para ello se debe emplear un archivo de texto como sistema de persistencia de datos.
---
+++ Clases/23_09_15-Actividad/Views/FrmAddAlumno.xaml.vb
@@ -0,0 +1,34 @@
+﻿Public Class FrmAddAlumno
+
+    Private frmAlumnos As FrmAlumnos
+
+    Public Sub New(frmAlumnos As FrmAlumnos)
+        InitializeComponent()
+        Me.frmAlumnos = frmAlumnos
+    End Sub
+
+    Private Sub BtnAddAlumno_Click(sender As Object, e As RoutedEventArgs) Handles BtnAddAlumno.Click
+        Dim opcionAño As String
+
+        Try
+            If RdBtn1.IsChecked Then
+                opcionAño = RdBtn1.Content.ToString()
+            ElseIf RdBtn2.IsChecked Then
+                opcionAño = RdBtn2.Content.ToString()
+            ElseIf RdBtn3.IsChecked Then
+                opcionAño = RdBtn3.Content.ToString()
+            Else
+                Throw New Exception("Must select an option")
+            End If
+
+            Dim dataAlumno As String = $"{TxtLegajo.Text},{TxtNombre.Text},{TxtApellido.Text},{opcionAño}"
+
+            TxtStudentService.AddAlumno(dataAlumno)
+            frmAlumnos.UpdateDataGrid()
+
+            Me.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
